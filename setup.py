123d1afa8641248eec34f072457f607c2539d899
Nora Belrose <belrose.nora@gmail.com>
2022-03-12 17:26:52 -0800

    Moved instructions to modal view

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version="0.1",
     description="Preference-based reinforcement learning in PyTorch and JAX with a browser-based GUI.",
     requires=[
-        "gym",
+        "gym~=0.22.0",  # 0.22.0 switched to PyGame for rendering
         "flask",
     ]
 )
